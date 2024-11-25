f8c67836f8acd24c176b8d69fc6f492c01dc3d3c
Mallory Miles <147207363+Cherry-Trees@users.noreply.github.com>
2024-11-24 23:52:21 -0600

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -8,25 +8,50 @@
 #include <iostream>
 #include <fstream>
 #include <unordered_map>
-#include "Graph.hpp"
+#include <unordered_set>
 #include "FibonacciHeap_Impl.hpp"
 #include "NNSet.hpp" // NNSet from Project 1.
 
-// Struct containing airport information.
-// Acts as a singly-linked list to track min-cost path.
-struct Record {
+// Edge struct.
+struct Edge {
+    std::string to;     // Vertex this edge is pointing to.
+    std::size_t weight; // Weight of this edge.
+
+    // Compare two edges (required for unordered_set).
+    bool operator==(const Edge &other) const {
+        return to == other.to && weight == other.weight;
+    }
+    // Hash the edge (required for unordered_set).
+    struct Hash {
+        static std::size_t operator()(const Edge &e) {
+            return std::hash<std::string>{}(e.to) ^ std::hash<std::size_t>{}(e.weight);
+        }
+    };
+};
+
+// Vertex struct containing airport information.
+// Also acts as a singly-linked list to track min-cost path.
+struct Vertex {
     std::string name;  // Airport name.
     std::size_t cost;  // Min-cost to travel here.
     std::size_t index; // Index to key with.
-    Record *prev;      // Hold the previous airport in the min-cost path.
+    Vertex *prev;      // Hold the previous airport in the min-cost path.
+    std::unordered_set<Edge, Edge::Hash> adj;   // Adjacency set.
     
-    Record(): name(), cost(0), prev(0) {}
-    Record(const std::string name, const std::size_t cost, const std::size_t index): name(name), cost(cost), index(index), prev(0) {}
+    Vertex(): 
+        name(), 
+        cost(0), 
+        prev(0) {}
+    Vertex(const std::string name, const std::size_t cost, const std::size_t index): 
+        name(name), 
+        cost(cost), 
+        index(index), 
+        prev(0) {}
 
-    // Key functor to specify which Record variable represents the key.
+    // Key functor to specify which Vertex variable represents the key.
     struct Key {
-        static std::size_t &operator()(Record &r) {return r.cost;}
-        static std::size_t &operator()(Record *r) {return r->cost;}
+        static std::size_t &operator()(Vertex &r) {return r.cost;}
+        static std::size_t &operator()(Vertex *r) {return r->cost;}
 
         // Comparison functor to specify how the min-heap will sort keys.
         struct Less {
@@ -36,14 +61,11 @@ struct Record {
         };
     };
 
-    // Heap node that this record is being stored in.
-    FibonacciHeap<Record *, Record::Key, Record::Key::Less>::Node *node;
+    // Heap node that this Vertex is being stored in.
+    FibonacciHeap<Vertex *, Vertex::Key, Vertex::Key::Less>::Node *node;
 };
 
 int main() {
-
-    // Construct graph object.
-    Graph flights;
 
     // Get file name, source name, and destination name.
     std::string file_name, src_name, dest_name;
@@ -61,9 +83,9 @@ int main() {
     std::ifstream in;
     in.open(file_name);
     
-    // Keep track of number of vertices and store records.
+    // Keep track of number of vertices and construct the graph.
     std::size_t V = 0;
-    std::unordered_map<std::string, Record> records; // Key by airport name.
+    std::unordered_map<std::string, Vertex> graph; // Key by airport name.
 
     // Read input file.
     while (!in.eof()) {
@@ -72,28 +94,29 @@ int main() {
         in >> from >> to >> cost;
 
         if (from.empty() || to.empty()) break;
-        if (flights.addVertex(from)) records.insert({from, Record(from, SIZE_MAX, V++)});
-        if (flights.addVertex(to)) records.insert({to, Record(to, SIZE_MAX, V++)});
-        flights.addEdge(from, to, cost + 2*airport_cost); // Cost to leave and arrive at airport = 2 * airport_cost
+        if (graph.insert({from, Vertex(from, SIZE_MAX, V)}).second) {++V;}
+        if (graph.insert({to,   Vertex(from, SIZE_MAX, V)}).second) {++V;}
+
+        graph[from].adj.insert({to, cost + 2*airport_cost});
+        graph[to].adj.insert({from, cost + 2*airport_cost});
     }
     in.close();
     
     // Construct the min heap.
-    FibonacciHeap<Record *, Record::Key, Record::Key::Less> heap;
+    FibonacciHeap<Vertex *, Vertex::Key, Vertex::Key::Less> heap;
 
     // Keep track of indices we've visited.
     NNSet visited(V);
 
-    // Get pointers to source and destination records for readability.
-    Record *src = &records[src_name];
-    Record *dest = &records[dest_name];
+    // Get pointers to source and destination graph for readability.
+    Vertex *src = &graph[src_name];
+    Vertex *dest = &graph[dest_name];
 
-    // Push all records to the heap.
-    for (auto &v_name : flights.getVertices()) {
-        Record *v = &records[v_name];
-        v->name = v_name;
-        v->cost = SIZE_MAX;
-        v->node = heap.push(v);
+    // Push all graph to the heap.
+    for (auto & [v_name, v] : graph) {
+        v.name = v_name;
+        v.cost = SIZE_MAX;
+        v.node = heap.push(&v);
     }
 
     // Set the source cost to 0.
@@ -102,17 +125,16 @@ int main() {
 
     // Dijkstra’s algorithm.
     while (!heap.empty()) {
-        Record *x = heap.pop(); // Pop the min record off the heap. This is an O(logn) operation.
-        visited.add(x->index); // Add the record to the visited set.
+        Vertex *x = heap.pop(); // Pop the min vertex off the heap. This is an O(logn) operation.
+        visited.add(x->index); // Add the vertex to the visited set.
 
         // Iterate through airports adj to x that haven't been visited.
-        for (auto &v_name : flights.getAdjacencies(x->name)) {
-            Record *v = &records[v_name]; // Get pointer to the record for readability.
+        for (auto &e : graph[x->name].adj) {
+            Vertex *v = &graph[e.to]; // Get pointer to the vertex for readability.
             if (!visited.contains(v->index)) { 
-                int weight = flights.getEdgeCost(x->name, v->name);
-                if (x->cost + weight < v->cost) {
-                    v->cost = x->cost + weight;
-                    v->prev = x; // Link the adj record to x.
+                if (x->cost + e.weight < v->cost) {
+                    v->cost = x->cost + e.weight;
+                    v->prev = x; // Link the adj vertex v to x.
                     heap.decrease_key(v->node, v->cost); // Decrease adj key in the heap.
                 }
             }
@@ -126,11 +148,11 @@ int main() {
 
     // Print min-cost path.
     // Since the path is linked dest --> ... --> src, print it using backwards recursion.
-    auto print_path = [](Record *r, bool parent, auto &&print_path) -> void {
-        if (r->prev)
-            print_path(r->prev, 0, print_path);
+    auto print_path = [](Vertex *v, bool parent, auto &&print_path) -> void {
+        if (v->prev)
+            print_path(v->prev, 0, print_path);
         std::string arrow = (parent) ? "" : " --> ";
-        std::cout << r->name << arrow;
+        std::cout << v->name << arrow;
     }; print_path(dest, 1, print_path);
 
     return 0;
